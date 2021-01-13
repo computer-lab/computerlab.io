456f1bf82771ca69be8ee03b970f82d2a9505bf0
Patrick Steadman <ptsteadman@gmail.com>
2021-01-13 00:05:23 -0500

    experiment w color
    Signed-off-by: Patrick Steadman <ptsteadman@gmail.com>

--- themes/computer-lab/source/js/app/heater.js
+++ themes/computer-lab/source/js/app/heater.js
@@ -7,13 +7,19 @@ define(['three'], function (THREE) {
   const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
   const canvas = document.getElementById('heater');
 
-  const renderer = new THREE.WebGLRenderer({ canvas });
+  const renderer = new THREE.WebGLRenderer({ canvas, alpha: true });
   renderer.setSize( window.innerWidth, window.innerHeight );
+  renderer.setClearColor(0x000000, 0);
 
   const geometry = new THREE.BoxGeometry();
-  const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+  const material = new THREE.MeshStandardMaterial( { color: 'hsl(20, 100%, 50%)' } );
   const cube = new THREE.Mesh( geometry, material );
   scene.add( cube );
+  const light = new THREE.PointLight( 0xffffff, 2, 100 );
+  light.position.set( 5, 5, 5 );
+  scene.add( light );
+  const ambient = new THREE.AmbientLight( 0x404040 ); // soft white light
+  scene.add( ambient );
 
   camera.position.z = 5;
 
